6f6802c30656e75b4ec4da4f8b97f38ba5019359
HierComsDatBoi <dainius.slekonis@gmail.com>
2024-07-05 15:53:40 +0300

    added buttons to close tabs, fixed overlay

--- git_ir_gitHub/main.js
+++ git_ir_gitHub/main.js
@@ -1,29 +1,51 @@
 // Menu buttons
 document.querySelector('#introBtn')
-.addEventListener('click', e => {
-e.preventDefault();
+.addEventListener('click', () => {
   document.querySelector('#mainPage').classList.add('hidden');
   document.querySelector('#introPage').classList.remove('hidden');
 });
 
 document.querySelector('#workBtn')
-.addEventListener('click', e => {
-e.preventDefault();
+.addEventListener('click', () => {
   document.querySelector('#mainPage').classList.add('hidden');
   document.querySelector('#workPage').classList.remove('hidden');
 });
 
 document.querySelector('#aboutBtn')
-.addEventListener('click', e => {
-e.preventDefault();
+.addEventListener('click', () => {
   document.querySelector('#mainPage').classList.add('hidden');
   document.querySelector('#aboutPage').classList.remove('hidden');
 });
 
 document.querySelector('#contactBtn')
-.addEventListener('click', e => {
-e.preventDefault();
+.addEventListener('click', () => {
   document.querySelector('#mainPage').classList.add('hidden');
   document.querySelector('#contactPage').classList.remove('hidden');
 });
 // menu buttons end
+
+
+//close buttons kiekvienai sekcijai
+document.querySelector('#closeIntro')
+.addEventListener('click', () =>{
+  document.querySelector('#introPage').classList.add('hidden');
+  document.querySelector('#mainPage').classList.remove('hidden');
+});
+
+document.querySelector('#closeWork')
+.addEventListener('click', () =>{
+  document.querySelector('#workPage').classList.add('hidden');
+  document.querySelector('#mainPage').classList.remove('hidden');
+});
+
+document.querySelector('#closeAbout')
+.addEventListener('click', () =>{
+  document.querySelector('#aboutPage').classList.add('hidden');
+  document.querySelector('#mainPage').classList.remove('hidden');
+});
+
+document.querySelector('#closeContact')
+.addEventListener('click', () =>{
+  document.querySelector('#contactPage').classList.add('hidden');
+  document.querySelector('#mainPage').classList.remove('hidden');
+});
